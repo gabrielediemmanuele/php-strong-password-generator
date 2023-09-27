e2ddd5505343a4c4399e7fccdbca4d06c86f556e
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-27 17:19:21 +0200

    add password screen and start write session

---
+++ password.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>PHP Strong Password Generator</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Gabriele Di Emmanuele" />
+    <meta name="description" content="php-strong-password-generator" />
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h2 class="text-success">Ecco la tua password!</h2>
+        <p></p>
+    </div>
+</body>
+
+</html>

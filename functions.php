fa9645572a8d0898cbc8cfb03d22416fe71237f2
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-27 17:00:35 +0200

    feat: add comments and include functions

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/*Funzione per generare la Password */
+function genPassword()
+{
+    /*creo una variabile > stringa con tutti i caratteri ammessi */
+    $legit_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!()-.?[]_~!:;@$%^&*+=';
+    /*creo una variabile che racchiuda i caratteri generati */
+    $gen_password = '';
+    /* imposto il valore minimo a 1*/
+    $i = 1;
+    /*creo una condizione.. */
+    /*se gen-password viene attivato... */
+    if (isset($_GET['gen-password'])) {
+        /*lunghezza della password è uguale al numero dato dall'utente*/
+        $password_length = $_GET['gen-password'];
+        /*creo un ciclo do e while*/
+        do {
+            /*creo una variabile per i caratteri che vengono generati*/
+            /*con rand prendo tot caratteri da 0 alla lunghezza dei legi - 1 */
+            $random_characters = $legit_characters[rand(0, strlen($legit_characters) - 1)];
+            /*richiamo con echo il random_caracters altrimenti non esiste..*/
+            echo $random_characters;
+            /*incremento il valore di 1 fin quando non raggiunge il massimo */
+            $i++;
+            /*la condizione è che il ciclo funzioni fin quando non è minore o uguale al valore dato dall'utente*/
+        } while ($i <= $password_length);
+    }
+    /*ritorno la password generata...*/
+    return $gen_password;
+}
+?>
+<!-- if ($password_length < 6 || $password_length > 15) {
+            
+        } -->
